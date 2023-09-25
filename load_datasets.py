782d25285f5c8194a4f0b9b7ab8cc766f4bde9e9
Arun Jayachandran <a.jayachandran@reply.de>
2023-09-26 01:06:27 +0200

    initial commit

--- load_datasets.py
+++ load_datasets.py
@@ -1,8 +1,8 @@
 from pandas import read_json
+from sqlalchemy import create_engine
 from bz2 import open as bz2_uncompresser
 
 with bz2_uncompresser("events.jsonl.bz2", 'rt', encoding='utf-8') as file:
-    print("2214324")
     df_eidu_data = read_json(file, lines=True)
 
 print(df_eidu_data.shape)
