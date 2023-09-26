fadb7d2e4c8eb9604cbb7c328f213fd55a008738
Arun Jayachandran <a.jayachandran@reply.de>
2023-09-26 03:27:41 +0200

    initial commit

--- load_data/load_dataset_to_redshift.py
+++ load_data/load_dataset_to_redshift.py
@@ -2,12 +2,7 @@ from pandas import read_json
 from sqlalchemy import create_engine
 from bz2 import open as bz2_uncompresser
 
-import os
-print(os.getcwd())
-print(os.listdir())
-print(os.listdir("../"))
+with bz2_uncompresser("load_data/events.jsonl.bz2", 'rt', encoding='utf-8') as file:
+    df_eidu_data = read_json(file, lines=True)
 
-#with bz2_uncompresser("transform_data/events.jsonl.bz2", 'rt', encoding='utf-8') as file:
-#    df_eidu_data = read_json(file, lines=True)
-
-#print(df_eidu_data.shape)
+print(df_eidu_data.shape)
